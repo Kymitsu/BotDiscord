a9d4080b177d4710e835675ec2d197a47a84e0e6
Etienne DIETRICH <etiennedietrich@gmail.com>
2020-09-13 14:13:15 +0200

    test modif pour avoir 2 random seed différentes

--- BotDiscord/RPG/DiceHelper.cs
+++ BotDiscord/RPG/DiceHelper.cs
@@ -6,15 +6,21 @@ namespace BotDiscord.RPG
 {
     public static class DiceHelper
     {
-        private static Random random = new Random();
+        private static Random random = null;
 
         public static DiceResult SimpleRoll(int dieSize, params int[] bonus)
         {
+            if (random == null)
+                random = new Random();
+
             return new DiceResult(new List<int> { random.Next(1, dieSize + 1) }, bonus);
         }
 
         public static int SimpleRoll(int dieSize)
         {
+            if (random == null)
+                random = new Random();
+
             return random.Next(1, dieSize + 1);
         }
     }
